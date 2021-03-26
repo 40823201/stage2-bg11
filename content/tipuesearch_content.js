bf9e2c137546493a49a09e6fa3597f6cb1e5fb6d
40823209 <40823209@gm.nfu.edu.tw>
2021-03-26 16:22:54 +0800

    加入小組倉儲

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '小組成員: \n 40823217 \n 40823201 \n 40823209 \n 40823210 \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '小組成員: \n 40823217 \n 40823201 \n 40823209:網站 \n 40823210 \n \n', 'tags': '', 'url': 'About.html'}]};
